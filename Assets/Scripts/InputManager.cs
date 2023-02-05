7cab69209f5965917a9bda71cad4f4bc2b909bbf
Pasquale Ambrosini <pasquale.ambrosini@gmail.com>
2023-02-05 04:11:06 +0100

    Test input

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,29 +5,23 @@ using static UnityEngine.InputSystem.InputAction;
 using UnityEngine.InputSystem;
 
 public class InputManager: MonoBehaviour {
+    public static int currentPlayers;
     public int playerIndex;
 
     void Start() {
         if (InputSingleton.current == null) {
             InputSingleton.current = new InputSingleton();
+            currentPlayers = 1;
+        } else {
+            currentPlayers += 1;
+            playerIndex = currentPlayers - 1;
         }
     }
 
     public void OnMove(CallbackContext context) {
         var type = context.control.device.path;
         if (!type.StartsWith("/Keyboard")) {
-            var hashCode = context.control.device.GetHashCode();
-            var player = InputSingleton.current.GetPlayerIndexFromController(hashCode);
-            if (player < 0) {
-                var freePlayer = InputSingleton.current.GetFirstFreePlayer();
-                if (freePlayer >= 0) {
-                    InputSingleton.current.controllers[freePlayer] = hashCode;
-                    player = freePlayer;
-                } else {
-                    player = 0;
-                }
-            }
-            UpdateMovement(context.ReadValue<Vector2>(), player);
+            UpdateMovement(context.ReadValue<Vector2>(), playerIndex);
         } else {
             UpdateMovement(context.ReadValue<Vector2>(), playerIndex);
         }
@@ -35,5 +29,10 @@ public class InputManager: MonoBehaviour {
 
     void UpdateMovement(Vector2 value, int player) {
         InputSingleton.current.dPads[player] = value;
+    }
+
+    public void OnPlayerJoined() {
+        Debug.Log("Connected");
+        GetComponent<PlayerInput>().enabled = true;
     }
 }
